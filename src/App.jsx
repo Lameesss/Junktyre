fcb6d1b64cf56e3392df6ed96095b2ba887458a9
Lamees <llameessss@gmail.com>
2025-08-25 16:37:58 +0530

    Fix import path for collection page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Collection from './pages/collection'
+// import Collection from './pages/collection'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import Product from './pages/Product'
-import Cart from './pages/Cart'
-import Login from './pages/Login'
-import PlaceOrder from './pages/PlaceOrder'
-import Orders from './pages/Orders'
+// import Product from './pages/Product'
+// import Cart from './pages/Cart'
+// import Login from './pages/Login'
+// import PlaceOrder from './pages/PlaceOrder'
+// import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Verify from './pages/Verify'
+// import Verify from './pages/Verify'
 
 
 const App = () => {
@@ -27,15 +27,15 @@ const App = () => {
       </div>
        <Routes>
           <Route path='/'  element={<Home/>}/>
-          <Route path='/collection'  element={<Collection/>}/>
+          {/* <Route path='/collection'  element={<Collection/>}/> */}
           <Route path='/about'  element={<About/>}/>
           <Route path='/contact'  element={<Contact/>}/>
-          <Route path='/product/:productId'  element={<Product/>}/>
-          <Route path='/cart'  element={<Cart/>}/>
-          <Route path='/login'  element={<Login/>}/>
-          <Route path='/place-order'  element={<PlaceOrder/>}/>
-          <Route path='/orders'  element={<Orders/>}/>
-          <Route path='/verify' element={<Verify/>}/>
+          {/* <Route path='/product/:productId'  element={<Product/>}/> */}
+          {/* <Route path='/cart'  element={<Cart/>}/> */}
+          {/* <Route path='/login'  element={<Login/>}/> */}
+          {/* <Route path='/place-order'  element={<PlaceOrder/>}/> */}
+          {/* <Route path='/orders'  element={<Orders/>}/>
+          <Route path='/verify' element={<Verify/>}/> */}
        </Routes>
        <Footer/>
     </div>
